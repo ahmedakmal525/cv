3f90e9ddc7b24c09e0879ed1f67e0e69d132948b
Ahmed Akmal <akmal.ahmed525@gmail.com>
2019-11-14 03:42:40 +0530

    Updates

---
+++ precache-manifest.b47a207ae0abea78074611639eff8619.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7ecde33f063698917d0c5257cc1c91",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "914c79698be636fa514d",
+    "url": "/cv/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "80c0d8df25f7dc6506b8",
+    "url": "/cv/static/css/main.ca36e0f5.chunk.css"
+  },
+  {
+    "revision": "914c79698be636fa514d",
+    "url": "/cv/static/js/2.787c18cb.chunk.js"
+  },
+  {
+    "revision": "80c0d8df25f7dc6506b8",
+    "url": "/cv/static/js/main.760b287f.chunk.js"
+  },
+  {
+    "revision": "15c1fbe4e3f13ef574e9",
+    "url": "/cv/static/js/runtime-main.3131271d.js"
+  },
+  {
+    "revision": "1897f35a7728f47d6c16afc7c50663b7",
+    "url": "/cv/static/media/AbhayaLibre-Regular.1897f35a.ttf"
+  },
+  {
+    "revision": "67f2f974993fd3c3e9e8e11589c1a0dc",
+    "url": "/cv/static/media/MuktaMalar-Regular.67f2f974.ttf"
+  },
+  {
+    "revision": "7775b2ab8bdaa25788912b375b09d9f4",
+    "url": "/cv/static/media/Questrial-Regular.7775b2ab.ttf"
+  },
+  {
+    "revision": "9709426e6d872048da7335a7b19bd768",
+    "url": "/cv/static/media/android.9709426e.png"
+  },
+  {
+    "revision": "14542f237bfe5a85401018b2e5431de7",
+    "url": "/cv/static/media/ansible.14542f23.png"
+  },
+  {
+    "revision": "2103c44598b4a6454d3df6e57b3d1415",
+    "url": "/cv/static/media/docker.2103c445.png"
+  },
+  {
+    "revision": "8587dd0647ca82fb4ca0372a93f2e358",
+    "url": "/cv/static/media/express.8587dd06.png"
+  },
+  {
+    "revision": "2d1998a6f2729f66ea655c0fd132d8a4",
+    "url": "/cv/static/media/flutter.2d1998a6.png"
+  },
+  {
+    "revision": "206233eb1f835d48bd2f7dd6ffb3df1e",
+    "url": "/cv/static/media/gradle.206233eb.png"
+  },
+  {
+    "revision": "2d0ef7ea2f9270a181df6e14eac79262",
+    "url": "/cv/static/media/java.2d0ef7ea.png"
+  },
+  {
+    "revision": "62c38a300ec97d401ea7fd1a4d948c18",
+    "url": "/cv/static/media/jenkins.62c38a30.png"
+  },
+  {
+    "revision": "4655ae6f8193d16e391f8971119decee",
+    "url": "/cv/static/media/kubernetes.4655ae6f.png"
+  },
+  {
+    "revision": "66add4c15e63120cea001e12d8823ad9",
+    "url": "/cv/static/media/loopback.66add4c1.png"
+  },
+  {
+    "revision": "be9251182f1c4d05f3f836cc3f39898b",
+    "url": "/cv/static/media/mysql.be925118.png"
+  },
+  {
+    "revision": "32d073d03bded75bc2a3b6074cc2c369",
+    "url": "/cv/static/media/node.32d073d0.png"
+  },
+  {
+    "revision": "39dff610661b307f38a28b07ff4c8ec0",
+    "url": "/cv/static/media/php.39dff610.png"
+  },
+  {
+    "revision": "086f2a4de4eea1e8bbf7a2dd49c4f66f",
+    "url": "/cv/static/media/postgres.086f2a4d.png"
+  },
+  {
+    "revision": "ad3f384361c29f34f00eb9fdecae91fd",
+    "url": "/cv/static/media/puppet.ad3f3843.png"
+  },
+  {
+    "revision": "067050f6ecc418ea7a7d2f3557683c8c",
+    "url": "/cv/static/media/python.067050f6.png"
+  },
+  {
+    "revision": "c53f360bc690f6f9c0c866de98190272",
+    "url": "/cv/static/media/rabbitmq.c53f360b.png"
+  },
+  {
+    "revision": "76118ce2a442bc51de0eb2023975099b",
+    "url": "/cv/static/media/react.76118ce2.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/cv/static/media/reactnative.47ce6e77.png"
+  },
+  {
+    "revision": "265c0c5099a5ca860941b9ae62f050ea",
+    "url": "/cv/static/media/vagrant.265c0c50.png"
+  },
+  {
+    "revision": "c30dd0ee9079a48bae6079b8da83329b",
+    "url": "/cv/static/media/vue.c30dd0ee.png"
+  }
+]);
